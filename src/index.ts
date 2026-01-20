0b7d56253af7baf554783f8f0090b0d335a3ee35
Leonid Osman <124144453+LorendosCSharp@users.noreply.github.com>
2026-01-20 22:32:03 +0100

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { Client,Unauthorized,ClientEvents } from './marcsync.js'
+export { SubscriptionManager,BaseEvent,EntryCreatedEvent,EntryDeletedEvent,EntryUpdatedEvent } from './SubscriptionManager.js'
+export { Collection,CollectionNotFound,CollectionAlreadyExists } from './Collection.js'
+export { BaseEntry,Entry,EntryData,EntryNotFound,EntryUpdateFailed } from './Entry.js'
+
